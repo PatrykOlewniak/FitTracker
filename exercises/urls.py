ae4e67b78dfc48fafcf3b6cd0c46e19e15395cbe
PatrykO <thawar@gmail.com>
2022-01-17 16:53:58 +0100

    feat: Update View and Url to support searching (filtering)

--- exercises/urls.py
+++ exercises/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('list/', views.ExerciseListView.as_view(), name='exercises'),
+    path('', views.ExerciseListView.as_view(), name='exercises'),
 ]
--- exercises/views.py
+++ exercises/views.py
@@ -1,14 +1,9 @@
-from django.http import HttpResponse
-from django.views import generic
+from tools.views import FilteredListView
 
+from .filters import ExerciseFilter
 from .models import Exercise
 
 
-def index(request):     # TEST view
-    ex_list = Exercise.objects.all()
-    output = '\n'.join(['<br>'.join((q.name, q.muscle_group)) for q in ex_list])
-    return HttpResponse(output)
-
-
-class ExerciseListView(generic.ListView):
+class ExerciseListView(FilteredListView):
     model = Exercise
+    filterset_class = ExerciseFilter
