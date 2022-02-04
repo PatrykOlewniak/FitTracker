e506b5e18868828c55a99f311f303d7dddf9d54d
PatrykO <thawar@gmail.com>
2022-02-04 23:04:10 +0100

    build: add new default db (postgres)

--- FitTracker/settings/base.py
+++ FitTracker/settings/base.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -65,9 +66,18 @@ WSGI_APPLICATION = 'FitTracker.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
+    },
+    'sql_lite': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+
 }
 
 
