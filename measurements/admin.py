f06face2fcbddb4ffea3dc014531e16f61b38df3
PatrykO <thawar@gmail.com>
2022-02-06 19:26:35 +0100

    style: Add todo for admin panel

--- measurements/admin.py
+++ measurements/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import *
 
-
+# TODO: change default user field for MeasureName (Django cant find a user with id 0 (treats null like 0)
 admin.site.register(MeasureName)
 admin.site.register(Measure)
 admin.site.register(BodyMeasure)
