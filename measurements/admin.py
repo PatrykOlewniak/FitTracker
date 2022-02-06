403fdc2ec2163e3c676b4e192aace44582e0fc93
PatrykO <thawar@gmail.com>
2022-02-06 18:48:00 +0100

    feat: add measurements model, basic admin panel

---
+++ measurements/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import *
+
+
+admin.site.register(MeasureName)
+admin.site.register(Measure)
+admin.site.register(BodyMeasure)
---
+++ measurements/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+from tools.mixins import CreatedAndUpdatedAtMixin
+
+
+class MeasureName(models.Model):
+    name = models.CharField(max_length=128)
+    generic = models.BooleanField(default=False)
+    created_by = models.ForeignKey(
+        User, related_name='body_parts', blank=True, null=True, default=False, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Measure(CreatedAndUpdatedAtMixin, models.Model):        # basic one, without split into weight and different types
+    class Unit(models.TextChoices):
+        KILOGRAM = 'KG', 'Kilogram'
+        CM = 'CM', 'Centimetre'
+
+    unit = models.CharField(max_length=2, choices=Unit.choices)
+    value = models.FloatField()
+    part = models.ForeignKey(MeasureName, related_name='parts_names', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f'{self.part.name} - {self.value}'
+
+
+class BodyMeasure(models.Model):
+    measure = models.ForeignKey(Measure, related_name='measures', on_delete=models.SET_NULL, null=True)
